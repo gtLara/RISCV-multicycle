b2cfc5472188046c293b49d5efce26ec02ad2081
Lara <gtlchaves@gmail.com>
2022-05-29 16:45:32 -0300

    Adiciona circuito para determinar quando tomar branch

--- components/brancher.vhd
+++ components/brancher.vhd
@@ -7,17 +7,34 @@ entity brancher is
     port(
     -- in
         funct3 : in std_logic_vector(2 downto 0);
-        negative : in std_logic ;
+        neg: in std_logic ;
         zero : in std_logic ;
     -- out
       	s_branch_taken : out std_logic
 	);
-end entity;
+end brancher;
 
 architecture brancher_arc of brancher is
-begin
-    process(negative, zero)
+
+    signal s_beq : std_logic ;
+    signal s_bne : std_logic ;
+    signal s_blt : std_logic ;
+    signal s_bge : std_logic ;
+
     begin
-        -- code goes here
+
+    process(funct3, neg, zero)
+        begin
+
+        s_beq <= (not funct3(2)) and (not funct3(1)) and (not funct3(0)) and zero;
+
+        s_bne <= (not funct3(2)) and (not funct3(1)) and (funct3(0)) and (not zero);
+
+        s_blt <= (funct3(2)) and (not funct3(1)) and (not funct3(0)) and neg;
+
+        s_bge <= (funct3(2)) and (not funct3(1)) and (funct3(0)) and (not neg or zero);
+
+        s_branch_taken <= s_beq or s_bne or s_blt or s_bge;
+
     end process;
 end brancher_arc;
