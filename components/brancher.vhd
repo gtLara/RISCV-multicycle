0937c53398074126d0a6c9750fcda0a25b7e5b81
Lara <gtlchaves@gmail.com>
2022-05-29 18:04:56 -0300

    Remove logica combinacional de processo

--- components/brancher.vhd
+++ components/brancher.vhd
@@ -20,21 +20,15 @@ architecture brancher_arc of brancher is
     signal s_bne : std_logic ;
     signal s_blt : std_logic ;
     signal s_bge : std_logic ;
+    signal novosinal : std_logic ;
 
     begin
 
-    process(funct3, neg, zero)
-        begin
-
         s_beq <= (not funct3(2)) and (not funct3(1)) and (not funct3(0)) and zero;
-
         s_bne <= (not funct3(2)) and (not funct3(1)) and (funct3(0)) and (not zero);
-
         s_blt <= (funct3(2)) and (not funct3(1)) and (not funct3(0)) and neg;
-
         s_bge <= (funct3(2)) and (not funct3(1)) and (funct3(0)) and (not neg or zero);
 
         s_branch_taken <= s_beq or s_bne or s_blt or s_bge;
 
-    end process;
 end brancher_arc;
