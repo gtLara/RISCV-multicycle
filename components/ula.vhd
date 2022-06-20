990a8497a19f25f75a7e8c1814a737ca3b3d7583
Lara <gtlchaves@gmail.com>
2022-06-20 00:01:44 -0300

    Adiciona xor

--- components/ula.vhd
+++ components/ula.vhd
@@ -14,15 +14,19 @@ entity ula is
     );
 
     port (
-        entrada_a : in std_logic_vector((largura_dado - 1) downto 0);
-        entrada_b : in std_logic_vector((largura_dado - 1) downto 0);
-        seletor   : in std_logic_vector(2 downto 0);
-        saida     : out std_logic_vector((largura_dado - 1) downto 0)
+        entrada_a : in std_logic_vector((largura_dado - 1) downto 0); 
+        entrada_b : in std_logic_vector((largura_dado - 1) downto 0); 
+        seletor   : in std_logic_vector(2 downto 0); 
+        saida     : out std_logic_vector((largura_dado - 1) downto 0); 
+        zero : out std_logic; 
+        negativo : out std_logic
     );
 end ula;
 
 architecture comportamental of ula is
     signal resultado_ula : std_logic_vector((largura_dado - 1) downto 0);
+    constant all_zeros : std_logic_vector(31 downto 0) := (others => '0');
+
 begin
     process (entrada_a, entrada_b, seletor) is
     begin
@@ -35,7 +39,7 @@ begin
             resultado_ula <= entrada_a and entrada_b;
             when "001" => -- or lógico
             resultado_ula <= entrada_a or entrada_b;
-            when "110" => -- xor lógico
+            when "100" => -- xor lógico
             resultado_ula <= entrada_a xor entrada_b;
             when "111" => -- not lógico
             resultado_ula <= not(entrada_a);
@@ -43,5 +47,12 @@ begin
             resultado_ula <= entrada_a xnor entrada_b;
         end case;
     end process;
+
     saida <= resultado_ula;
+
+    zero <= '1' when resultado_ula = all_zeros else
+            '0';
+
+    negativo <= resultado_ula(31); -- hi se resultado de operacao for negativo
+
 end comportamental;
